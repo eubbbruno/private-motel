5e22be3bae11a2e5119a55cc6920c7be7dcad1d6
BrunoB <84853589+eubbbruno@users.noreply.github.com>
2025-07-01 20:14:52 -0300

    fix: Reorganiza homepage - Suites apos Hero, remove popup e secao parceiros

--- app/page.js
+++ app/page.js
@@ -1,6 +1,5 @@
 import Header from '../src/components/Header';
 import HeroSection from '../src/components/sections/HeroSection';
-import MotorReservaSection from '../src/components/sections/MotorReservaSection';
 import GaleriaSuitesSection from '../src/components/sections/GaleriaSuitesSection';
 import ExperienciasSection from '../src/components/sections/ExperienciasSection';
 import RefeicoesSection from '../src/components/sections/RefeicoesSection';
@@ -8,7 +7,6 @@ import PromocoesSection from '../src/components/sections/PromocoesSection';
 import SobreNosSection from '../src/components/sections/SobreNosSection';
 import NewsletterSection from '../src/components/sections/NewsletterSection';
 import Footer from '../src/components/Footer';
-import PopupDesconto from '../src/components/PopupDesconto';
 import PopupCookies from '../src/components/PopupCookies';
 import styles from './page.module.css';
 
@@ -19,7 +17,6 @@ export default function Home() {
       <Header />
       <main className={styles.main}>
         <HeroSection />
-        <MotorReservaSection />
         <GaleriaSuitesSection />
         <ExperienciasSection />
         <RefeicoesSection />
@@ -28,7 +25,6 @@ export default function Home() {
         <NewsletterSection />
       </main>
       <Footer />
-      <PopupDesconto />
       <div className={styles.particles}>
         <div className={styles.particle}></div>
         <div className={styles.particle}></div>
